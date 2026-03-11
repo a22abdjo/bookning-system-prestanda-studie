eaafaa5c29c8e3878d04968302e250cc0f53f76b
datguywelbs <a22abdjo@student.his.se>
2026-03-11 14:12:41 +0100

    Lägg till express server och statisk frontend-servering

--- public/app.js
+++ public/app.js
@@ -1,3 +1,3 @@
 async function loadBookings(dbType) {
-    document.getElementById("time").textContent = `Knappen för ${dbType}fungerar`;
+    document.getElementById("time").textContent = `Knappen för ${dbType} fungerar`;
 }
