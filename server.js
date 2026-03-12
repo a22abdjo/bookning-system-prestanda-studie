e500eeb6ccee3e378ab8f04ae36fa0c2e8f9b552
datguywelbs <a22abdjo@student.his.se>
2026-03-12 17:28:09 +0100

    Lägg till MySQL-endpoints för bokning, sökning och historik

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 
 const express = require("express");
 const path = require("path");
+const { getBookingsHistory, searchBookingsByName } = require("./mysql");
 
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/api/test", (req, res) => {
@@ -14,6 +16,44 @@ app.get("/api/test", (req, res) => {
         message: `API works with ${dbType}`
     });
 });
+
+app.get("/api/mysql/history", async (req, res) => {
+    try {
+      const data = await getBookingsHistory();
+      res.json(data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "MySQL history error"});
+    } 
+});
+
+app.get("/api/mysql/search", async (req, res) => {
+    try {
+      const name = req.query.name || "";  
+      const data = await searchBookingsByName(name);
+      res.json(data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "MySQL search error"});
+    } 
+});
+
+app.post("/api/mysql/book", async (req, res) => {
+    try {
+      const name = req.query.name || "";  
+
+      const data = await searchBookingsByName(name);
+
+      res.json({
+        message: "bookings created in MySQL",
+        insertId: result.insertId
+      });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "MySQL booking error"});
+    } 
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost${PORT}`);
